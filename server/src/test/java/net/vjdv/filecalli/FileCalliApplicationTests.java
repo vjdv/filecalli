4260e5de46ab0fe40d33f374b6226adb04b80649
Jassiel <25399314+vjdv@users.noreply.github.com>
2024-04-01 11:50:24 -0600

    spring initializr

---
+++ server/src/test/java/net/vjdv/filecalli/FileCalliApplicationTests.java
@@ -0,0 +1,13 @@
+package net.vjdv.filecalli;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FileCalliApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
